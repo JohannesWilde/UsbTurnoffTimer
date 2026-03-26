0eb1fb22751c86e1fcc7166913c15dbfd166c0aa
Johannes Wilde <git@jowil.de>
2026-03-26 23:49:47 +0100

    Make the LED at the devBoard blink.
    Even though once again it does not match the "documentation" of the seller. The LED is at P1_2 and not P5_5. And the LED lights up when P1_2 is HIGH (1) - not if it is low as per the "documentation".

--- src/main.c
+++ src/main.c
@@ -21,10 +21,10 @@ static void delay(unsigned int t)
 
 void main()
 {
-    P5M0 = (P5M0 & ~(0b1 << 5)) | (DIO_MODE_BIDIRECTIONAL_M0 << 5);
-    P5M1 = (P5M1 & ~(0b1 << 5)) | (DIO_MODE_BIDIRECTIONAL_M1 << 5);
+    P1M0 = (P5M0 & ~(0b1 << 2)) | (DIO_MODE_PUSH_PULL_OUTPUT_M0 << 2);
+    P1M1 = (P5M1 & ~(0b1 << 2)) | (DIO_MODE_PUSH_PULL_OUTPUT_M1 << 2);
 
-    P5_5 = 1; // LED at dev board.
+    P1_2 = 1; // LED at dev board.
     // WKTCL = 0xFE; // Set the power-down wake-up clock to be about 10 seconds
     // WKTCH = 0x87;
     // EA = 1;
@@ -32,7 +32,7 @@ void main()
     while (1)
     {
         // PCON |= 0x02;  // Enter power-down mode
-        P5_5 = (0 != P5_5) ? 0 : 1;  // Toggle P5.5
-        delay(500);
+        P1_2 = (0 != P1_2) ? 0 : 1;  // Toggle P5.5
+        delay(50);
     }
 }
