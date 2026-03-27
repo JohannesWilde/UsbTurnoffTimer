a64c75fc4e29453f9dc8affd7eb0fcdfbcd4ebd6
Johannes Wilde <git@jowil.de>
2026-03-27 08:16:06 +0100

    Create pin name via macros.

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@ static void delay(unsigned int t)
   }
 }
 
+#define MAKE_PIN_NAME_(port, pin) P##port##_##pin
+#define MAKE_PIN_NAME(port, pin) MAKE_PIN_NAME_(port, pin)
+
 #define DIO_MODE_BIDIRECTIONAL_M0 0
 #define DIO_MODE_BIDIRECTIONAL_M1 0
 #define DIO_MODE_PUSH_PULL_OUTPUT_M0 1
@@ -18,21 +21,25 @@ static void delay(unsigned int t)
 #define DIO_MODE_OPEN_DRAIN_M0 1
 #define DIO_MODE_OPEN_DRAIN_M1 1
 
+#define LED_PORT_NUMBER 1
+#define LED_PIN_NUMBER 2
+
+#define LED_PIN MAKE_PIN_NAME(LED_PORT_NUMBER, LED_PIN_NUMBER)
 
 void main()
 {
-    P1M0 = (P5M0 & ~(0b1 << 2)) | (DIO_MODE_PUSH_PULL_OUTPUT_M0 << 2);
-    P1M1 = (P5M1 & ~(0b1 << 2)) | (DIO_MODE_PUSH_PULL_OUTPUT_M1 << 2);
+    P1M0 = (P5M0 & ~(0b1 << LED_PIN_NUMBER)) | (DIO_MODE_PUSH_PULL_OUTPUT_M0 << LED_PIN_NUMBER);
+    P1M1 = (P5M1 & ~(0b1 << LED_PIN_NUMBER)) | (DIO_MODE_PUSH_PULL_OUTPUT_M1 << LED_PIN_NUMBER);
 
-    P1_2 = 1; // LED at dev board.
-    // WKTCL = 0xFE; // Set the power-down wake-up clock to be about 10 seconds
-    // WKTCH = 0x87;
-    // EA = 1;
+    LED_PIN = 1; // LED at dev board.
+    WKTCL = 0xFE; // Set the power-down wake-up clock to be about 10 seconds
+    WKTCH = 0x87;
+    EA = 1; // enable interrupts
 
     while (1)
     {
-        // PCON |= 0x02;  // Enter power-down mode
-        P1_2 = (0 != P1_2) ? 0 : 1;  // Toggle P5.5
-        delay(50);
+        PCON |= 0x02;  // Enter power-down mode
+        LED_PIN = (0 != LED_PIN) ? 0 : 1;  // Toggle P5.5
+        // delay(50);
     }
 }
