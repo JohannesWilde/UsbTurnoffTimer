d119f1694900abe07e597b973d939ae8c0249f62
Johannes Wilde <git@jowil.de>
2026-03-28 11:06:15 +0100

    Make push button control LED directly.

--- src/main.c
+++ src/main.c
@@ -24,10 +24,17 @@ static void delay(unsigned int t)
 #define LED_PORT_NUMBER 1
 #define LED_PIN_NUMBER 2
 
+#define PUSH_BUTTON_PORT_NUMBER 3
+#define PUSH_BUTTON_PIN_NUMBER 4  // for final layout 0 - but right now avoid conflicts with programming pins.
+
 #define LED_PIN MAKE_PIN_NAME(LED_PORT_NUMBER, LED_PIN_NUMBER)
+#define PUSH_BUTTON_PIN MAKE_PIN_NAME(PUSH_BUTTON_PORT_NUMBER, PUSH_BUTTON_PIN_NUMBER)
 
 void main()
 {
+    P3M0 = (P3M0 & ~(0b1 << PUSH_BUTTON_PIN_NUMBER)) | (DIO_MODE_HIGH_Z_INPUT_M0 << PUSH_BUTTON_PIN_NUMBER);
+    P3M1 = (P3M1 & ~(0b1 << PUSH_BUTTON_PIN_NUMBER)) | (DIO_MODE_HIGH_Z_INPUT_M1 << PUSH_BUTTON_PIN_NUMBER);
+
     P1M0 = (P5M0 & ~(0b1 << LED_PIN_NUMBER)) | (DIO_MODE_PUSH_PULL_OUTPUT_M0 << LED_PIN_NUMBER);
     P1M1 = (P5M1 & ~(0b1 << LED_PIN_NUMBER)) | (DIO_MODE_PUSH_PULL_OUTPUT_M1 << LED_PIN_NUMBER);
 
@@ -38,8 +45,10 @@ void main()
 
     while (1)
     {
-        PCON |= 0x02;  // Enter power-down mode
-        LED_PIN = (0 != LED_PIN) ? 0 : 1;  // Toggle P5.5
+        // PCON |= 0x02;  // Enter power-down mode
+        // LED_PIN = (0 != LED_PIN) ? 0 : 1;  // Toggle P5.5
         // delay(50);
+
+        LED_PIN = PUSH_BUTTON_PIN;
     }
 }
