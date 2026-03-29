0b04b9fc848f2ab79c0a2ee8dd8af2b9caba7596
Johannes Wilde <git@jowil.de>
2026-03-29 20:28:14 +0200

    Make the LCD count the seconds and minutes.

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
 
 #define F_IRC 24000000ull  // Hz
@@ -252,23 +253,24 @@ void tm1637DisplayCommand(bool const on, uint8_t const brightness)
 
 typedef enum
 {
-    n0 = 0,
-    n1 = 1,
-    n2 = 2,
-    n3 = 3,
-    n4 = 4,
-    n5 = 5,
-    n6 = 6,
-    n7= 7,
-    n8= 8,
-    n9= 9,
-    a = 10,
-    b = 11,
-    c = 12,
-    d = 13,
-    e = 14,
-    f = 15,
-    minus = 16,
+    tm1637Character_0 = 0,
+    tm1637Character_1 = 1,
+    tm1637Character_2 = 2,
+    tm1637Character_3 = 3,
+    tm1637Character_4 = 4,
+    tm1637Character_5 = 5,
+    tm1637Character_6 = 6,
+    tm1637Character_7= 7,
+    tm1637Character_8= 8,
+    tm1637Character_9= 9,
+    tm1637Character_a = 10,
+    tm1637Character_b = 11,
+    tm1637Character_c = 12,
+    tm1637Character_d = 13,
+    tm1637Character_e = 14,
+    tm1637Character_f = 15,
+    tm1637Character_minus = 16,
+    tm1637Character_none = 17,
 } Tm1637Character;
 
 
@@ -310,7 +312,50 @@ static uint8_t const tm1637Characters[] =
     0b01111001,    // E
     0b01110001,    // F
     0b01000000,    // -
+    0b00000000,    //
 };
+
+void tm1637RenderTime(Duration const * const duration)
+{
+    if ((1000ull * 60ull * 60ull * 100ull) <= *duration)
+    {
+        // memset(tm1637DisplayData, tm1637Characters[tm1637Character_minus], 4);
+        tm1637DisplayData[0] = tm1637Characters[tm1637Character_minus];
+        tm1637DisplayData[1] = (tm1637DisplayData[1] & 0x80) | tm1637Characters[tm1637Character_minus];
+        tm1637DisplayData[2] = tm1637Characters[tm1637Character_minus];
+        tm1637DisplayData[3] = tm1637Characters[tm1637Character_minus];
+    }
+    else
+    {
+        Duration durationCopy = *duration;
+        // milliseconds -> seconds
+        durationCopy /= 1000;
+        uint8_t const seconds = durationCopy % 60;
+        // seconds -> minutes
+        durationCopy /= 60;
+        uint8_t const minutes = durationCopy % 60;
+        // minutes -> hours
+        durationCopy /= 60;
+        uint8_t const hours = durationCopy;
+
+        tm1637DisplayData[0] = tm1637Characters[minutes / 10];
+        tm1637DisplayData[1] = (tm1637DisplayData[1] & 0x80) | tm1637Characters[minutes % 10];
+        tm1637DisplayData[2] = tm1637Characters[seconds / 10];
+        tm1637DisplayData[3] = tm1637Characters[seconds % 10];
+    }
+}
+
+void tm1637RenderColon(bool const enabled)
+{
+    if (enabled)
+    {
+        tm1637DisplayData[1] |= (1 * 0x80); // colon
+    }
+    else
+    {
+        tm1637DisplayData[1] &= ~(1 * 0x80); // colon
+    }
+}
 
 
 void main()
@@ -415,8 +460,6 @@ void main()
 
             PWR_SWITCH_PIN = newValue;  // Toggle P5.5
 
-
-
             if (0 != newValue)
             {
                 tm1637DisplayData[1] |= (1 * 0x80); // colon
@@ -426,7 +469,13 @@ void main()
                 tm1637DisplayData[1] &= ~(1 * 0x80); // colon
             }
 
-            tm1637AddressCommand(/*address*/ 1, &tm1637DisplayData[1], /*count*/ 1);
+            // tm1637AddressCommand(/*address*/ 1, &tm1637DisplayData[1], /*count*/ 1);
+
+
+            Duration const duration = millis();
+            tm1637RenderTime(&duration);
+
+            tm1637AddressCommand(/*address*/ 0, tm1637DisplayData, /*count*/ 4);
 
         }
 
