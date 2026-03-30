a3642bc59313edbbba0a7b55f57d701128842460
Johannes Wilde <git@jowil.de>
2026-03-30 14:39:13 +0200

    Use positive value for modulo operations.

--- src/main.c
+++ src/main.c
@@ -415,9 +415,9 @@ void tm1637RenderNumberSigned(int8_t const number)
           tm1637DisplayData[0] = tm1637Characters[tm1637Character_none];
       }
 
-      tm1637DisplayData[1] = (tm1637DisplayData[1] & 0x80) | tm1637Characters[(number / 100) /*% 10*/];
-      tm1637DisplayData[2] = tm1637Characters[(number / 10) % 10];
-      tm1637DisplayData[3] = tm1637Characters[number % 10];
+      tm1637DisplayData[1] = (tm1637DisplayData[1] & 0x80) | tm1637Characters[(numberCopy / 100) /*% 10*/];
+      tm1637DisplayData[2] = tm1637Characters[(numberCopy / 10) % 10];
+      tm1637DisplayData[3] = tm1637Characters[numberCopy % 10];
     }
 }
 
