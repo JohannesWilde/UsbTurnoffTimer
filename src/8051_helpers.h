415c575f4f16f6b24e870cc047dd034a0bfcdb51
Johannes Wilde <git@jowil.de>
2026-03-31 09:15:40 +0200

    Move DIO_MODE helpers out.

--- src/8051_helpers.h
+++ src/8051_helpers.h
@@ -4,6 +4,16 @@
 #include <mcs51/8051.h>
 
 
+#define DIO_MODE_BIDIRECTIONAL_M0 0
+#define DIO_MODE_BIDIRECTIONAL_M1 0
+#define DIO_MODE_PUSH_PULL_OUTPUT_M0 1
+#define DIO_MODE_PUSH_PULL_OUTPUT_M1 0
+#define DIO_MODE_HIGH_Z_INPUT_M0 0
+#define DIO_MODE_HIGH_Z_INPUT_M1 1
+#define DIO_MODE_OPEN_DRAIN_M0 1
+#define DIO_MODE_OPEN_DRAIN_M1 1
+
+
 inline void noInterrupts()
 {
     EA = 0;
